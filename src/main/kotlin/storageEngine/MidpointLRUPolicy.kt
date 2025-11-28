e18f216d40c4a48b66c54b5e2b066ac3d294eca6
HJinS <windowed@naver.com>
2025-11-28 18:48:30 +0900

    - addNode 추가   - 기존에 있던 중복된 함수 제거   - MidpointLRUPolicy 리펙토링 필요

--- src/main/kotlin/storageEngine/MidpointLRUPolicy.kt
+++ src/main/kotlin/storageEngine/MidpointLRUPolicy.kt
@@ -1,5 +1,13 @@
 package storageEngine
 
+
+/**
+ * ```
+ *       ---------       ---------       ---------       ---------       ---------       ---------
+ * ------|  head  |------|        |------|        |------|  mid   |------|        |------|  tail  |------
+ *       ---------       ---------       ---------       ---------       ---------       ---------
+ * ```
+ * */
 class MidpointLRUPolicy(private val capacity: Int): ReplacementPolicy{
     internal class Node(val key: Int, var prev: Node? = null, var next: Node? = null)
     private val map = HashMap<Int, Node>()
@@ -27,7 +35,7 @@ class MidpointLRUPolicy(private val capacity: Int): ReplacementPolicy{
                 midPoint = node.next!!
             }
             remove(node)
-            addToNew(node)
+            addNode(node, head.next!!)
         // 없는 경우는 새로운 노드를 만들어서 midpoint 에 삽입
         }else{
             if(map.size >= capacity){
@@ -35,7 +43,8 @@ class MidpointLRUPolicy(private val capacity: Int): ReplacementPolicy{
             }
             // 남은 공간 확인 후 자리가 없으면 evict 필요
             val newNode = Node(key)
-            addToMidpoint(newNode)
+            addNode(newNode, midPoint)
+            midPoint = newNode
             map[key] = newNode
         }
     }
@@ -57,19 +66,14 @@ class MidpointLRUPolicy(private val capacity: Int): ReplacementPolicy{
         midPoint = if(midPoint == node) nextNode else midPoint
     }
 
-    private fun addToNew(node: Node){
-        val preFirstNode = head.next!!
-        node.prev = head
-        node.next = preFirstNode
-        head.next = node
-        preFirstNode.prev = node
+    /**
+     * add [node] to the left of [targetNode]
+     * */
+    private fun addNode(node: Node, targetNode: Node){
+        val prevNode = targetNode.prev!!
+        prevNode.next = node
+        targetNode.prev = node
+        node.next = targetNode
     }
 
-    private fun addToMidpoint(node: Node){
-        val newListTail = midPoint.prev!!
-        newListTail.next = node
-        midPoint.prev = node
-        node.next = midPoint
-        midPoint = node
-    }
 }
