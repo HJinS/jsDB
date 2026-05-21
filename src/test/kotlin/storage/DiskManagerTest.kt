4f8a7c4eecc8e8b5271900ed667c17883990eda0
HJinS <windowed@naver.com>
2026-05-21 14:56:03 +0900

    test: DiskManager 테스트 코드 수정및 실패 케이스 수정.
    - DiskManager Error 클래스 추가
    - partial Read/Write 대응

--- src/test/kotlin/storage/DiskManagerTest.kt
+++ src/test/kotlin/storage/DiskManagerTest.kt
@@ -6,6 +6,7 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import storageEngine.DiskManager
+import storageEngine.exception.DiskManagerException
 import java.io.File
 import java.nio.ByteBuffer
 
@@ -18,41 +19,49 @@ class DiskManagerTest: BehaviorSpec({
     }
 
     given("disk manager with empty file"){
-        `when`("read page with pageId zero"){
-            val buffer = ByteBuffer.allocate(PAGE_SIZE)
-            then("should throw an IllegalArgumentException"){
+        `when`("read page with buffer size PAGE_SIZE/2"){
+            val buffer = ByteBuffer.allocate(PAGE_SIZE/2)
+            then("should throw an InvalidReadOffsetException"){
                 val error = shouldThrow<IllegalArgumentException> { diskManager.readPage(0, buffer) }
-                error.message shouldBe "Negative position"
+                error.message shouldBe "Page data must be exactly $PAGE_SIZE bytes, but got ${buffer.capacity()}"
+            }
+        }
+
+        `when`("read page from an empty disk"){
+            val buffer = ByteBuffer.allocate(PAGE_SIZE)
+            then("should throw an InvalidReadOffsetException"){
+                shouldThrow<DiskManagerException.InvalidReadOffsetException> { diskManager.readPage(0, buffer) }
             }
         }
 
         `when`("write page with pageId zero"){
             val data = 1239238
             val buffer = ByteBuffer.allocate(PAGE_SIZE).putInt(data)
-            then("should throw an IllegalArgumentException"){
-                val error = shouldThrow<IllegalArgumentException> { diskManager.writePage(0, buffer) }
-                error.message shouldBe "Negative position"
+            diskManager.writePage(0L, buffer)
+            then("current total page should be 1"){
+                val totalPage = diskManager.getNumPages()
+                totalPage shouldBe 1
             }
         }
+
         `when`("read page 1 from empty database file"){
             val buffer = ByteBuffer.allocate(PAGE_SIZE)
-            then("should throw an IllegalArgumentException"){
-                val error = shouldThrow<IllegalArgumentException> { diskManager.readPage(2, buffer) }
-                error.message shouldBe "No such page"
+            then("should throw an InvalidReadOffsetException"){
+                shouldThrow<DiskManagerException.InvalidReadOffsetException> { diskManager.readPage(1, buffer) }
             }
         }
         `when`("write data"){
             val data = 1239238
             val buffer = ByteBuffer.allocate(PAGE_SIZE).putInt(data)
             diskManager.writePage(1, buffer)
-            then("write action ends successfully."){
-                diskManager.getNumPages() shouldBe 1
+            then("current total page should be 2"){
+                diskManager.getNumPages() shouldBe 2
             }
 
             val byteBufferForRead = ByteBuffer.allocate(PAGE_SIZE)
             diskManager.readPage(1, byteBufferForRead)
             then("read result should be same"){
-                byteBufferForRead.getInt(0) shouldBe data
+                byteBufferForRead.getInt() shouldBe data
             }
         }
     }
