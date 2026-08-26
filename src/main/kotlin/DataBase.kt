7e4da7ad26d7bb059f1823f1c1b5c443994ee137
HJinS <windowed@naver.com>
2026-08-26 16:48:58 +0900

    - table, column, index 생성 함수 추가   - 각 카탈로그의 rootPageId 업데이트 callback 함수 추가   - 일반 인덱스는 별도의 rootPageId 업데이트 callback 필요(카탈로그에서 업데이트 필요함)

--- src/main/kotlin/DataBase.kt
+++ src/main/kotlin/DataBase.kt
@@ -1,31 +1,96 @@
+import catalog.CatalogManager
+import catalog.data.ColumnRow
 import config.SimpleConfig
+import exception.DatabaseException
 import index.btree.BTree
-import index.serializer.KeySerializer
-import index.serializer.ValueSerializer
+import index.serializer.BinaryRowSerializer
+import index.serializer.MultiColumnKeySerializer
+import index.util.IndexKeySchema
+import index.util.RowSchema
 import storageEngine.BufferPoolManager
-import storageEngine.DatabaseInitializer
+import storageEngine.MetaPageManager
 import storageEngine.DiskManager
 import storageEngine.FreeSpaceManager
 import storageEngine.StorageManager
 import storageEngine.lru.FrameNodePolicy
+import storageEngine.util.MetaPageOffset
+import util.requireOrThrow
+import kotlin.String
 
 
 class DataBase(private val config: SimpleConfig) {
     private val diskManager = DiskManager(config.storageConfig, config.indexConfig)
     private val lruPolicy = FrameNodePolicy(config.storageConfig.midPointLruConfig)
     private val bufferPoolManager = BufferPoolManager(diskManager, lruPolicy, config.indexConfig, config.storageConfig.poolSize)
-    private val databaseInitializer = DatabaseInitializer(bufferPoolManager)
+    private val metaPageManager = MetaPageManager(bufferPoolManager)
     private val freeSpaceManager = FreeSpaceManager(bufferPoolManager)
     private val storageManager = StorageManager(freeSpaceManager, bufferPoolManager, config.indexConfig)
+    private lateinit var catalogManager: CatalogManager
 
-    fun initialize() = databaseInitializer.initMetaPage()
+    fun initialize() {
+        val metaPageData = metaPageManager.initialize()
+        catalogManager = CatalogManager(
+            metaPageData = metaPageData,
+            storageManager = storageManager,
+            indexConfig = config.indexConfig,
+            onIndexCatalogRootChanged = { newRoot ->
+                metaPageManager.updateRootPageId(MetaPageOffset.INDEX_CATALOG_ROOT_PAGE_ID, newRoot)
+            },
+            onTableCatalogRootChanged = { newRoot ->
+                metaPageManager.updateRootPageId(MetaPageOffset.TABLE_CATALOG_ROOT_PAGE_ID, newRoot)
+            },
+            onColumnCatalogRootChanged = { newRoot ->
+                metaPageManager.updateRootPageId(MetaPageOffset.COLUMN_CATALOG_ROOT_PAGE_ID, newRoot)
+            }
+        )
+    }
 
-    fun <K, V> createIndex(
-        name: String,
+    fun createIndex(
+        indexName: String,
+        tableId: Long,
+        isPrimary: Boolean,
         targetTable: String,
-        keySerializer: KeySerializer<K>,
-        valueSerializer: ValueSerializer<V>,
-    ): BTree<K, V> {
-        return BTree(name, targetTable, storageManager, keySerializer, valueSerializer, config.indexConfig)
+        isUnique: Boolean,
+        keySchema: IndexKeySchema,
+        rowSchema: RowSchema,
+    ): BTree<List<Any?>, List<Any?>> {
+        val resolved = catalogManager.resolveIndex(indexName)
+        requireOrThrow(resolved == null) { DatabaseException.IndexAlreadyExistsException(indexName) }
+        val indexId = metaPageManager.getNextId(MetaPageOffset.NEXT_INDEX_ID)
+        catalogManager.registerNewIndex(indexId, indexName, tableId, null, isPrimary, isUnique, keySchema.indexColumns)
+        return BTree(
+            indexName,
+            targetTable,
+            storageManager,
+            MultiColumnKeySerializer(keySchema),
+            BinaryRowSerializer(rowSchema),
+            config.indexConfig,
+-1L
+        )
+    }
+
+    fun createTable(tableName: String, primaryIndexKeySchema: IndexKeySchema, columns: RowSchema){
+        val resolved = catalogManager.resolveTable(tableName)
+        requireOrThrow(resolved == null) { DatabaseException.TableAlreadyExistsException(tableName) }
+        val tableId = metaPageManager.getNextId(MetaPageOffset.NEXT_TABLE_ID)
+        val tableRow = catalogManager.registerNewTable(tableId, tableName, null)
+        for((idx, column) in columns.rowColumns.withIndex()){
+            createColumn(tableRow.tableId, idx, column.name, column.type.toString(), column.nullable)
+        }
+        createIndex(
+            "${tableName}_PK_IDX",
+            tableRow.tableId,
+            true,
+            tableRow.tableName,
+            true,
+            primaryIndexKeySchema,
+            columns
+        )
+    }
+
+    fun createColumn(tableId: Long, ordinal: Int, name: String, type: String, nullable: Boolean): ColumnRow{
+        val resolved = catalogManager.resolveColumn(tableId, ordinal)
+        requireOrThrow(resolved == null) { DatabaseException.ColumnAlreadyExistsException(tableId, name) }
+        return catalogManager.registerNewColumn(tableId, ordinal, name, type, nullable)
     }
 }
---
+++ src/main/kotlin/catalog/CatalogManager.kt
@@ -0,0 +1,108 @@
+package catalog
+
+import catalog.data.ColumnRaw
+import catalog.data.ColumnRow
+import catalog.data.IndexRaw
+import catalog.data.IndexRow
+import catalog.data.TableRaw
+import catalog.data.TableRow
+import catalog.exception.CatalogException
+import config.IndexConfig
+import index.btree.BTree
+import index.serializer.BinaryRowSerializer
+import index.serializer.MultiColumnKeySerializer
+import index.util.ColumnType
+import index.util.IndexColumn
+import storageEngine.StorageManager
+
+class CatalogManager(
+    metaPageData: MetaPageData,
+    private val storageManager: StorageManager,
+    private val indexConfig: IndexConfig,
+    onTableCatalogRootChanged: (Long) -> Unit,
+    onIndexCatalogRootChanged: (Long) -> Unit,
+    onColumnCatalogRootChanged: (Long) -> Unit,
+) {
+    private var tableCatalog = BTree(
+        name = CatalogBoot.TABLE_CATALOG_INDEX_NAME,
+        targetTable = CatalogBoot.TABLE_CATALOG_NAME,
+        storageManager = storageManager,
+        keySerializer = MultiColumnKeySerializer(CatalogBoot.TABLE_CATALOG_KEY),
+        valueSerializer = BinaryRowSerializer(CatalogBoot.TABLE_CATALOG_ROW),
+        indexConfig = indexConfig,
+        rootPageId = metaPageData.tableCatalogRootPageId,
+        onRootChanged = onTableCatalogRootChanged
+    )
+    private var indexCatalog = BTree(
+        name = CatalogBoot.INDEX_CATALOG_INDEX_NAME,
+        targetTable = CatalogBoot.INDEX_CATALOG_NAME,
+        storageManager = storageManager,
+        keySerializer = MultiColumnKeySerializer(CatalogBoot.INDEX_CATALOG_KEY),
+        valueSerializer = BinaryRowSerializer(CatalogBoot.INDEX_CATALOG_ROW),
+        indexConfig = indexConfig,
+        rootPageId = metaPageData.indexCatalogRootPageId,
+        onRootChanged = onIndexCatalogRootChanged
+    )
+    private var columnCatalog = BTree(
+        name = CatalogBoot.COLUMN_CATALOG_INDEX_NAME,
+        targetTable = CatalogBoot.COLUMN_CATALOG_NAME,
+        storageManager = storageManager,
+        keySerializer = MultiColumnKeySerializer(CatalogBoot.COLUMN_CATALOG_KEY),
+        valueSerializer = BinaryRowSerializer(CatalogBoot.COLUMN_CATALOG_ROW),
+        indexConfig = indexConfig,
+        rootPageId = metaPageData.columnCatalogRootPageId,
+        onRootChanged = onColumnCatalogRootChanged
+    )
+
+    fun resolveIndex(name: String): IndexRow?{
+        val data = indexCatalog.search(listOf(name)) ?: return null
+        return IndexRaw(data).toRow()
+    }
+
+    fun resolveColumn(tableId: Long, ordinal: Int): ColumnRow?{
+        val data = columnCatalog.search(listOf(tableId, ordinal)) ?: return null
+        return ColumnRaw(data).toRow()
+    }
+
+    fun resolveTable(name: String): TableRow?{
+        val data = tableCatalog.search(listOf(name)) ?: return null
+        return TableRaw(data).toRow()
+    }
+
+    fun registerNewColumn(tableId: Long, ordinal: Int, name: String, type: String, nullable: Boolean): ColumnRow{
+        val columnType = try{
+            ColumnType.valueOf(type)
+        } catch (e: IllegalArgumentException){
+            throw CatalogException.CorruptedCatalogException(CatalogBoot.COLUMN_CATALOG_NAME, e)
+        }
+        columnCatalog.insert(
+            listOf(tableId, ordinal),
+            listOf(tableId, ordinal, name, type, nullable)
+        )
+        return ColumnRow(tableId, ordinal, name, columnType, nullable)
+    }
+
+    fun registerNewTable(tableId: Long, tableName: String, primaryIndexId: Long?): TableRow{
+        tableCatalog.insert(
+            listOf(tableName),
+            listOf(tableId, tableName, primaryIndexId)
+        )
+        return TableRow(tableId, tableName, primaryIndexId)
+    }
+
+    fun registerNewIndex(
+        indexId: Long,
+        indexName: String,
+        tableId: Long,
+        rootPageId: Long?,
+        isPrimary: Boolean,
+        isUnique: Boolean,
+        keyColumns: List<IndexColumn>
+    ): IndexRow{
+        indexCatalog.insert(
+            listOf(indexName),
+            listOf(indexId, indexName, tableId, rootPageId, isPrimary, isUnique, keyColumns.encodeKeyColumns())
+        )
+        return IndexRow(indexId, indexName, tableId, rootPageId, isPrimary, isUnique, keyColumns)
+    }
+}
