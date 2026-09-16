5fb1638b91a066a7e32deca4c3cdc632df40386c
HJinS <windowed@naver.com>
2026-09-16 23:17:10 +0900

    lint 정리

--- src/main/kotlin/exception/IndexException.kt
+++ src/main/kotlin/exception/IndexException.kt
@@ -7,34 +7,34 @@ sealed class IndexException(
     sqlState: SqlState,
     detail: EngineErrorDetail,
     cause: Throwable? = null
-): RuntimeException(detail.toMessage(sqlState), cause) {
-    class InvalidTraceStack(detail: EngineErrorDetail, cause: Throwable? = null):
+) : RuntimeException(detail.toMessage(sqlState), cause) {
+    class InvalidTraceStack(detail: EngineErrorDetail, cause: Throwable? = null) :
         IndexException(SqlState.INTERNAL_ERROR, detail, cause)
 
-    class EmptyTree(detail: EngineErrorDetail, cause: Throwable? = null):
+    class EmptyTree(detail: EngineErrorDetail, cause: Throwable? = null) :
         IndexException(SqlState.INTERNAL_ERROR, detail, cause)
 
-    class InvalidNodeType(detail: EngineErrorDetail, cause: Throwable? = null):
+    class InvalidNodeType(detail: EngineErrorDetail, cause: Throwable? = null) :
         IndexException(SqlState.INTERNAL_ERROR, detail, cause)
 
-    class InvalidSafeCheck(detail: EngineErrorDetail, cause: Throwable? = null):
+    class InvalidSafeCheck(detail: EngineErrorDetail, cause: Throwable? = null) :
         IndexException(SqlState.INTERNAL_ERROR, detail, cause)
 
-    class InvalidBytes(detail: EngineErrorDetail, cause: Throwable? = null):
+    class InvalidBytes(detail: EngineErrorDetail, cause: Throwable? = null) :
         IndexException(SqlState.INTERNAL_ERROR, detail, cause)
 
-    class PositionOutOfBounds(detail: EngineErrorDetail, cause: Throwable? = null):
+    class PositionOutOfBounds(detail: EngineErrorDetail, cause: Throwable? = null) :
         IndexException(SqlState.INTERNAL_ERROR, detail, cause)
 
-    class VarIntTooLong(detail: EngineErrorDetail, cause: Throwable? = null):
+    class VarIntTooLong(detail: EngineErrorDetail, cause: Throwable? = null) :
         IndexException(SqlState.INTERNAL_ERROR, detail, cause)
 
-    class InvalidUUIDLength(detail: EngineErrorDetail, cause: Throwable? = null):
+    class InvalidUUIDLength(detail: EngineErrorDetail, cause: Throwable? = null) :
         IndexException(SqlState.INTERNAL_ERROR, detail, cause)
 
-    class InvalidTraceObject(detail: EngineErrorDetail, cause: Throwable? = null):
+    class InvalidTraceObject(detail: EngineErrorDetail, cause: Throwable? = null) :
         IndexException(SqlState.INTERNAL_ERROR, detail, cause)
 
-    class LeafNodeNotFound(detail: EngineErrorDetail, cause: Throwable? = null):
+    class LeafNodeNotFound(detail: EngineErrorDetail, cause: Throwable? = null) :
         IndexException(SqlState.INTERNAL_ERROR, detail, cause)
 }
